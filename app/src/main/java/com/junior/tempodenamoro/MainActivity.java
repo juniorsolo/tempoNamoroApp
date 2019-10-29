cca359bc648030758e375079427e036df3891bc0
Manoel <manoel.junior.ext@sascar.com>
2019-10-29 18:57:58 -0300

    Adicionando icon.

--- app/src/main/java/com/junior/tempodenamoro/MainActivity.java
+++ app/src/main/java/com/junior/tempodenamoro/MainActivity.java
@@ -6,6 +6,10 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+
 public class MainActivity extends AppCompatActivity {
 
     private ViewHolder mViewHolder = new ViewHolder();
@@ -18,15 +22,23 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textCurrentTime = findViewById(R.id.current_time);
         this.mViewHolder.buttonNextBirthday = findViewById(R.id.button_next_birthday);
 
-        this.mViewHolder.textCurrentTime.setText("");
+        this.mViewHolder.textCurrentTime.setText( this.findTimeDating() );
 
-        this.findTimeDating();
+
     }
 
-    private void findTimeDating(){
+    private String findTimeDating(){
+        LocalDate dateDating = LocalDate.of(2017, Month.OCTOBER,20);
+        LocalDate today = LocalDate.now();
+        Period period = Period.between(dateDating, today);
 
+        String years = period.getYears() > 1 ? period.getYears() + " anos " : period.getYears() + " ano ";
+        String months = period.getMonths() > 1 ? period.getMonths() + " meses " : period.getMonths() + " mês e ";
+        String days = period.getDays() > 1 ? period.getDays() + " dias " : period.getDays() + " dia";
 
+        String msg = years + months + days;
 
+        return msg;
     }
     private static class ViewHolder{
         TextView textCurrentTime;
