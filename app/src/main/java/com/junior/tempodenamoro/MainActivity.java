676f77917745d72c39483b0c3b5056d561b02f27
Manoel <manoel.junior.ext@sascar.com>
2019-10-29 17:34:59 -0300

    Criando layout da tela principal.

--- app/src/main/java/com/junior/tempodenamoro/MainActivity.java
+++ app/src/main/java/com/junior/tempodenamoro/MainActivity.java
@@ -1,14 +1,35 @@
 package com.junior.tempodenamoro;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.textCurrentTime = findViewById(R.id.current_time);
+        this.mViewHolder.buttonNextBirthday = findViewById(R.id.button_next_birthday);
+
+        this.mViewHolder.textCurrentTime.setText("");
+
+        this.findTimeDating();
+    }
+
+    private void findTimeDating(){
+
+
+
+    }
+    private static class ViewHolder{
+        TextView textCurrentTime;
+        Button buttonNextBirthday;
     }
 }
